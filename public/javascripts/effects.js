2d57cf87eef4da973af583369a3b324834b354db
Kendra Lyndon <kendralyndon@gmail.com>
2016-09-15 21:10:01 -0600

    more styling

--- public/javascripts/effects.js
+++ public/javascripts/effects.js
@@ -1,18 +1,25 @@
 $(document).ready(function(){
   var link = $('a'),
-  button = $('.btn');
+  button = $('.btn'),
+  headerButton = $('.header-btn')
   link.on('mouseenter',function(){
     $(this).css('color','#B2417A');
     $(this).css('border-color','#B2417A');
   });
   link.on('mouseleave',function(){
     $(this).css('color','#eeeeee');
-    $(this).css('border-color','#eeeeee');
+    $(this).css('border-color','#F2F2F2');
+  })
+  headerButton.on('mouseenter',function(){
+    $(this).css('color','#B2417A');
+  });
+  headerButton.on('mouseleave',function(){
+    $(this).css('color','#F2F2F2');
   })
   button.on('mouseenter',function(){
     $(this).css('background-color','#B2417A');
   });
   button.on('mouseleave',function(){
-    $(this).css('background-color','#eeeeee');
+    $(this).css('background-color','#F2F2F2');
   })
 })
