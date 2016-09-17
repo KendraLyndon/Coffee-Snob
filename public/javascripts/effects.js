9b2c189326e7a30f5b293ca9b012f4caeab10db1
Kendra Lyndon <kendralyndon@gmail.com>
2016-09-16 21:51:10 -0600

    change front page layout

--- public/javascripts/effects.js
+++ public/javascripts/effects.js
@@ -1,7 +1,25 @@
 $(document).ready(function(){
   var link = $('a'),
-  button = $('.btn'),
-  headerButton = $('.header-btn')
+      button = $('.btn'),
+      headerButton = $('.header-btn'),
+      loginForm = $('#login-form'),
+      logIn = $('#log-in-btn'),
+      signupForm = $('#signup-form'),
+      signUp = $('#signup-btn');
+
+  $(loginForm).hide();
+  $(signupForm).hide();
+
+  $(logIn).click(function(){
+    $(signupForm).hide();
+    $(loginForm).toggle();
+  })
+
+  $(signUp).click(function(){
+    $(loginForm).hide();
+    $(signupForm).toggle();
+  })
+
   link.on('mouseenter',function(){
     $(this).css('color','#B2417A');
     $(this).css('border-color','#B2417A');
@@ -17,9 +35,9 @@ $(document).ready(function(){
     $(this).css('color','#F2F2F2');
   })
   button.on('mouseenter',function(){
-    $(this).css('background-color','#B2417A');
+    $(this).css('background-color','#8C5A67');
   });
   button.on('mouseleave',function(){
-    $(this).css('background-color','#F2F2F2');
+    $(this).css('background-color','#59261B');
   })
 })
