88f0ef9bf87e2388d31b4227fa05e48bec3d3d3f
Kendra Lyndon <kendralyndon@gmail.com>
2016-12-09 13:47:10 -0700

    fix typo

--- routes/users.js
+++ routes/users.js
@@ -27,13 +27,11 @@ router.post('/', function(req, res, next) {
     if(user){
       res.redirect('/');
     } else {
-      if(Validations.validateUser(user)){
-        Users.add(req.body).then(function(){
-          Users.find(req.body).then(function(user){
-            res.cookie('user_id',user.id);
-            res.cookie('user_name',user.user_name);
-            res.redirect('/');
-          })
+      Users.add(req.body).then(function(){
+        Users.find(req.body).then(function(user){
+          res.cookie('user_id',user.id);
+          res.cookie('user_name',user.user_name);
+          res.redirect('/');
         })
       })
     }
